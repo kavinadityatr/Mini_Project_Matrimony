d9ed47e9226d3a6227999b180e675a5c886be45e
Kavin Aditya <99775748+kavinadityatr@users.noreply.github.com>
2023-07-12 15:30:00 +0530

    Add files via upload

---
+++ D2/matrimony/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Body from './components/Body';
+import Login from './components/Login';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Login/>
+      
+    </div>
+  );
+}
+
+export default App;
